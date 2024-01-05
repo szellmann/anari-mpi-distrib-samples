92bc5e276602b1dabd11a421f76e559bfbd01db7
Stefan Zellmann <info@szellmann.de>
2024-01-05 02:41:37 +0100

    missing file

---
+++ util/Manip.h
@@ -0,0 +1,12 @@
+
+#pragma once
+
+namespace util {
+
+  struct Manip
+  {
+    enum class MouseButton { Left, Right, Middle, };
+    enum class ModifierKey { CTRL, ALT, Shift, None, };
+  };
+
+} // namespace util
